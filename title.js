1b64d8c8ef4cc4ccfaf22c1387acbe3bfbae8f5f
Microhard_1724 <122979224+microharddemo@users.noreply.github.com>
2023-01-22 15:12:04 +0800

    Create title.js

---
+++ title.js
@@ -0,0 +1,15 @@
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', 'https://v1.hitokoto.cn');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var data = JSON.parse(xhr.responseText);
+      var hitokoto = document.getElementById('hitokoto_text');
+      hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
+      if(data.from_who == null){
+        data.from_who = "";
+      }
+      hitokoto.innerText = "『 " +data.hitokoto +" 』";
+      hitokoto_from.innerText = "—— " +data.from_who +"「" +data.from +"」";
+    }
+  }
+  xhr.send();
